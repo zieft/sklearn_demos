c7856649434c7da2a51c900edc51128fe08c4b91
Yulin Zhu <zieft@hotmail.com>
2017-03-25 21:41:16 +0100

    add some examples

--- Text_data/CountVectorizer().py
+++ Text_data/CountVectorizer().py
@@ -1,4 +1,10 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.datasets import fetch_20newsgroups
+
+categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+
+
 
 vectorizer = CountVectorizer()
 string1 = 'hi Katie the self driving car will be late Best Sebastian'
@@ -9,3 +15,12 @@ bag_of_words = vectorizer.fit(email_list)
 print bag_of_words
 bag_of_words = vectorizer.transform(email_list)
 print bag_of_words
+
+from nltk.corpus import stopwords
+sw = stopwords.words('english')
+
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer('english')
+stemmer.stem('responsiveness')
+stemmer.stem('responsivity')
+
