4ee2b25154b3e10163a908bf7f2a5714bbb46844
Yulin Zhu <zieft@hotmail.com>
2017-05-07 20:09:54 +0200

    download datasets

--- Classification/Classification.py
+++ Classification/Classification.py
@@ -3,7 +3,9 @@ import numpy.random as rnd
 import os
 import matplotlib
 import matplotlib.pyplot as plt
-from
+import six.moves
+import sklearn.datasets
+import scipy.io
 
 plt.rcParams["axes.labelsize"] = 14
 plt.rcParams["xtick.labelsize"] = 12
@@ -19,3 +21,23 @@ def save_fig(fig_id, tight_layout=True):
         plt.tight_layout()
     plt.savefig(path, format='png', dpi=300)
 
+
+try:
+    mnist = sklearn.datasets.fetch_mldata('MNIST original')
+except six.moves.urllib.error.HTTPError as ex:
+    print("Could not download MNIST data from mldata.org, trying alternative....")
+
+    mnist_alternative_url = "https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat"
+    mnist_path = "./mnist-original.mat"
+    response = six.move.urllib.request.urlopen(mnist_alternative_url)
+    with open(mnist_path, 'wb') as f:
+        content = response.read()
+        f.write(content)
+    mnist_raw = scipy.io.loadmat(mnist_path)
+    mnist = {
+        "data": mnist_raw["data"].T,
+        "target": mnist_raw["label"][0],
+        "COL_NAMES": ["label", "data"],
+        "DESCR": "mldata.org dataset: mnist-original",
+    }
+    print("Success!")
