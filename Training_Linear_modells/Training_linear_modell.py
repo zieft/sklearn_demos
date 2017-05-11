ac415c0970dc01a4ffff707d2a384d9a2894d86b
Yulin Zhu <zieft@hotmail.com>
2017-05-11 17:31:24 +0200

    add mbgd

--- Training_Linear_modells/Training_linear_modell.py
+++ Training_Linear_modells/Training_linear_modell.py
@@ -156,5 +156,46 @@ sgd_reg = sklearn.linear_model.SGDRegressor()
 sgd_reg.fit(X, y.ravel())
 print(sgd_reg.intercept_, sgd_reg.coef_)
 
-
 # Mini-batch Gradient Descent
+theta_path_mgd = []
+
+n_iterations = 50
+minibatch_size = 20
+
+theta = rnd.randn(2, 1)
+
+t0, t1, = 10, 1000
+
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+
+t = 0
+for epoch in range(n_iterations):
+    shuffled_indices = rnd.permutation(m)
+    X_b_shuffled = X_b[shuffled_indices]
+    y_shuffled = y[shuffled_indices]
+    for i in range(0, m, minibatch_size):
+        t += 1
+        xi = X_b_shuffled[i:i+minibatch_size]
+        yi = y_shuffled[i:i+minibatch_size]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(t)
+        theta = theta - eta * gradients
+        theta_path_mgd.append(theta)
+
+theta_path_bgd = np.array(theta_path_bgd)
+theta_path_sgd = np.array(theta_path_sgd)
+theta_path_mgd = np.array(theta_path_mgd)
+
+plt.figure(figsize=(7, 4))
+plt.plot(theta_path_sgd[:, 0], theta_path_sgd[:, 1], "r-s", linewidth=1, label="Stochastic")
+plt.plot(theta_path_mgd[:, 0], theta_path_mgd[:, 1], "g-+", linewidth=2, label="Mini-batch")
+plt.plot(theta_path_bgd[:, 0], theta_path_bgd[:, 1], "b-o", linewidth=3, label="Batch")
+plt.legend(loc="upper left", fontsize=16)
+plt.xlabel(r"$\theta_0$", fontsize=20)
+plt.ylabel(r"$\theta_1$   ", fontsize=20, rotation=0)
+plt.axis([2.5, 4.5, 2.3, 3.9])
+save_fig("gradient_descent_paths_plot")
+
