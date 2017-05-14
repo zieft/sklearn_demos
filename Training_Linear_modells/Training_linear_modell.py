09297cd61fe369f249ed17322a74696a31ee4596
Yulin Zhu <zieft@hotmail.com>
2017-05-14 16:44:10 +0200

    add 2 features

--- Training_Linear_modells/Training_linear_modell.py
+++ Training_Linear_modells/Training_linear_modell.py
@@ -566,7 +566,7 @@ y = (iris["target"] == 2).astype(np.int)
 log_reg = sklearn.linear_model.LogisticRegression()
 log_reg.fit(X, y)
 
-# now look at the model's estimated probabilities for flowers with petal widths
+# now look at the model's estimated probabilities for flowers with petal widths (only 1 features)
 # varying from 0 to 3cm
 X_new = np.linspace(0, 3, 1000).reshape(-1, 1)  # .reshape()参数中的负号：
 y_proba = log_reg.predict_proba(X_new)
@@ -587,3 +587,38 @@ plt.legend(loc="center left", fontsize=14)
 plt.axis([0, 3, -0.02, 1.02])
 save_fig("logistic_regression_plot")
 
+print(log_reg.predict([[1.7], [1.5]]))
+
+# now same dataset but 2 features: petal width and length.
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(np.int)
+
+log_reg = sklearn.linear_model.LogisticRegression(C=10 ** 10)  # C: The higher the C, the less the model is regularized.
+log_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+    np.linspace(2.9, 7, 500).reshape(-1, 1),  # reshape(-1,1)改写成列向量
+    np.linspace(0.8, 2.7, 200).reshape(-1, 1)
+)
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+y_proba = log_reg.predict_proba(X_new)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y == 0, 0], X[y == 0, 1], "bs")
+plt.plot(X[y == 1, 0], X[y == 1, 1], "g^")
+
+zz = y_proba[:, 1].reshape(x0.shape)
+contour = plt.contour(x0, x1, zz, cmap=plt.cm.brg)
+
+left_right = np.array([2.9, 7])
+boundary = -(log_reg.coef_[0][0] * left_right + log_reg.intercept_[0]) / log_reg.coef_[0][1]
+
+plt.clabel(contour, inline=1, fontsize=12)
+plt.plot(left_right, boundary, "k--", linewidth=3)
+plt.text(3.5, 1.5, "Not Iris-Virginica", fontsize=14, color="b", ha="center")
+plt.text(6.5, 2.3, "Iris-Virginica", fontsize=14, color="g", ha="center")
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.axis([2.9, 7, 0.8, 2.7])
+save_fig("logistic_regression_contour_plot")
