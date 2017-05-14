f6c04c4c3163a1803e422e5bfce66c7e537bd77b
Yulin Zhu <zieft@hotmail.com>
2017-05-14 16:29:50 +0200

    add logistic regression

--- Training_Linear_modells/Training_linear_modell.py
+++ Training_Linear_modells/Training_linear_modell.py
@@ -8,6 +8,7 @@ import sklearn.linear_model
 import sklearn.preprocessing
 import sklearn.pipeline
 import sklearn.base
+import sklearn.datasets
 
 rnd.seed(42)
 plt.rcParams["axes.labelsize"] = 14
@@ -537,3 +538,52 @@ for subplot in (223, 224):
     plt.xlabel(r"$\theta_1$", fontsize=20)
 
 save_fig("lasso_vs_ridge_plot")
+
+# Logistic Regression
+# Estimating Probabilities
+# Plot of a logistic function sigma_of_t
+t = np.linspace(-10, 10, 100)
+sig = 1 / (1 + np.exp(-t))
+plt.figure(figsize=(9, 3))
+plt.plot([-10, 10], [0, 0], "k-")
+plt.plot([-10, 10], [0.5, 0.5], "k:")
+plt.plot([-10, 10], [1, 1], "k:")
+plt.plot([0, 0], [-1.1, 1.1], "k-")
+plt.plot(t, sig, "b-", linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
+plt.xlabel("t")
+plt.legend(loc="upper left", fontsize=20)
+plt.axis([-10, 10, -0.1, 1.1])
+save_fig("logistic_function_plot")
+
+# try to build a classifier to detect the Iris-Virginica type
+iris = sklearn.datasets.load_iris()
+print(list(iris.keys()))
+print(iris.DESCR)
+
+X = iris["data"][:, 3:]  # petal width
+y = (iris["target"] == 2).astype(np.int)
+
+log_reg = sklearn.linear_model.LogisticRegression()
+log_reg.fit(X, y)
+
+# now look at the model's estimated probabilities for flowers with petal widths
+# varying from 0 to 3cm
+X_new = np.linspace(0, 3, 1000).reshape(-1, 1)  # .reshape()参数中的负号：
+y_proba = log_reg.predict_proba(X_new)
+decision_boundary = X_new[y_proba[:, 1] >= 0.5][0]
+
+plt.figure(figsize=(8, 3))
+plt.plot(X[y == 0], y[y == 0], "bs")
+plt.plot(X[y == 1], y[y == 1], "g^")
+plt.plot([decision_boundary, decision_boundary], [-1, 2], "k:", linewidth=2)
+plt.plot(X_new, y_proba[:, 1], "g-", linewidth=2, label="Iris-Virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", linewidth=2, label="Not Iris-Virginica")
+plt.text(decision_boundary + 0.02, 0.15, "Decision boundary", fontsize=14, color="k", ha="center")
+plt.arrow(decision_boundary, 0.08, -0.3, 0, head_width=0.05, head_length=0.1, fc="b", ec="b")
+plt.arrow(decision_boundary, 0.92, 0.3, 0, head_width=0.05, head_length=0.1, fc="g", ec="g")
+plt.xlabel("Petal width (cm)", fontsize=14)
+plt.ylabel("Probability", fontsize=14)
+plt.legend(loc="center left", fontsize=14)
+plt.axis([0, 3, -0.02, 1.02])
+save_fig("logistic_regression_plot")
+
