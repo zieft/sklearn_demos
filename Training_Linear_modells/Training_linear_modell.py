f83d9e2dd1b3148cccb3c7cf523ee0811e61c928
Yulin Zhu <zieft@hotmail.com>
2017-05-14 13:53:04 +0200

    add early stopping

--- Training_Linear_modells/Training_linear_modell.py
+++ Training_Linear_modells/Training_linear_modell.py
@@ -364,6 +364,61 @@ lasso_reg = sklearn.linear_model.Lasso(alpha=0.1)
 lasso_reg.fit(X, y)
 print(lasso_reg.predict([[1.5]]))
 
-
 # Elastic Net
+elastic_net = sklearn.linear_model.ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
+
+# Early Stopping
+rnd.seed(42)
+m = 100
+X = 6 * rnd.rand(m, 1) - 3
+y = 2 + X + 0.5 * X ** 2 + rnd.randn(m, 1)
+
+X_train, X_val, y_train, y_val = sklearn.model_selection.train_test_split(X[:50], y[:50].ravel(), test_size=0.5,
+                                                                          random_state=10)
+
+poly_scaler = sklearn.pipeline.Pipeline([
+    ("poly_features", sklearn.preprocessing.PolynomialFeatures(degree=90, include_bias=False)),
+    ("std_scaler", sklearn.preprocessing.StandardScaler()),
+])
+
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+sgd_reg = sklearn.linear_model.SGDRegressor(n_iter=1,
+                                            penalty=None,
+                                            eta0=0.0005,
+                                            warm_start=True,
+                                            learning_rate="constant",
+                                            random_state=42)
+
+n_epochs = 500
+train_errors, val_errors = [], []
+for epoch in range(n_epochs):
+    sgd_reg.fit(X_train_poly_scaled, y_train)
+    y_train_predict = sgd_reg.predict(X_train_poly_scaled)
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    train_errors.append(sklearn.metrics.mean_squared_error(y_train_predict, y_train))
+    val_errors.append(sklearn.metrics.mean_squared_error(y_val_predict, y_val))
+
+best_epoch = np.argmin(val_errors)
+best_val_rmse = np.sqrt(val_errors[best_epoch])
+
+plt.annotate('Best model',
+             xy=(best_epoch, best_val_rmse),
+             xytext=(best_epoch, best_val_rmse + 1),
+             ha="center",
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             fontsize=16,
+             )
+
+best_val_rmse -= 0.03  # just to make the graph look better
+plt.plot([0, n_epochs], [best_val_rmse, best_val_rmse], "k:", linewidth=2)
+plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="Validation set")
+plt.plot(np.sqrt(train_errors), "r--", linewidth=2, label="Training set")
+plt.legend(loc="upper right", fontsize=14)
+plt.xlabel("Epoch", fontsize=14)
+plt.ylabel("RMSE", fontsize=14)
+save_fig("early_stopping_plot")
 
