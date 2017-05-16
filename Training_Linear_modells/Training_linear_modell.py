971fbbeafb53005d83ba273f17bc5468dee85b14
Yulin Zhu <zieft@hotmail.com>
2017-05-16 18:14:58 +0200

    add softmax regression

--- Training_Linear_modells/Training_linear_modell.py
+++ Training_Linear_modells/Training_linear_modell.py
@@ -4,6 +4,7 @@ import numpy.linalg
 import os
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import sklearn.linear_model
 import sklearn.preprocessing
 import sklearn.pipeline
@@ -235,7 +236,7 @@ plt.plot(X_new, y_new, 'r-', linewidth=2, label="predictions")
 plt.xlabel("$x_1$", fontsize=18)
 plt.ylabel("$y$", rotation=0, fontsize=18)
 plt.legend(loc="upper left", fontsize=14)
-plt.axis([-3, 3.0, 10])
+plt.axis([-3, 3, 0, 10])
 save_fig("quadratic_predictions_plot")
 
 # Compare the performances of different-degree Polynomial Regression model
@@ -597,6 +598,8 @@ log_reg = sklearn.linear_model.LogisticRegression(C=10 ** 10)  # C: The higher t
 log_reg.fit(X, y)
 
 x0, x1 = np.meshgrid(
+    # 生成petal width 从0.8 到2.7，和
+    # petal length 从 2.9到7厘米的样本。
     np.linspace(2.9, 7, 500).reshape(-1, 1),  # reshape(-1,1)改写成列向量
     np.linspace(0.8, 2.7, 200).reshape(-1, 1)
 )
@@ -622,3 +625,46 @@ plt.xlabel("Petal length", fontsize=14)
 plt.ylabel("Petal width", fontsize=14)
 plt.axis([2.9, 7, 0.8, 2.7])
 save_fig("logistic_regression_contour_plot")
+
+# Softmax Regression
+# classify the iris flowers into all three classes
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = iris["target"]
+
+softmax_reg = sklearn.linear_model.LogisticRegression(multi_class="multinomial", solver="lbfgs", C=10)
+"""
+multi_class="multinomial"  # switch to Softmax Regression instead of one vs all
+solver="lbfgs"  # specified solver that supports Softmax Regression
+C=10  # change the regularization method.
+"""
+softmax_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+    np.linspace(0, 8, 500).reshape(-1, 1),
+    np.linspace(0, 3.5, 200).reshape(-1, 1),
+)
+
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+y_proba = softmax_reg.predict_proba(X_new)
+y_predict = softmax_reg.predict(X_new)
+
+zz1 = y_proba[:, 1].reshape(x0.shape)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y == 2, 0], X[y == 2, 1], "g^", label="Iris-Virginica")
+plt.plot(X[y == 1, 0], X[y == 1, 1], "bs", label="Iris-Versicolor")
+plt.plot(X[y == 0, 0], X[y == 0, 1], 'yo', label="Iris-Setosa")
+
+custom_cmap = matplotlib.colors.ListedColormap(["#fafab0", "#9898ff", "#a0faa0"])
+
+plt.contourf(x0, x1, zz, cmap=custom_cmap, linewidth=5)
+contour = plt.contour(x0, x1, zz1, cmap=plt.cm.brg)
+plt.clabel(contour, inline=1, fontsize=12)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="center left", fontsize=14)
+plt.axis([0, 7, 0, 3.5])
+save_fig("softmax_regression_contour_plot")
