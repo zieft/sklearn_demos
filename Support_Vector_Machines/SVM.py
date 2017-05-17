1dedc61d11db05dfec25d551d15031492f9e99f0
Yulin Zhu <zieft@hotmail.com>
2017-05-17 21:00:41 +0200

    add sensitivity to feature scales

--- Support_Vector_Machines/SVM.py
+++ Support_Vector_Machines/SVM.py
@@ -6,6 +6,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import sklearn.svm
 import sklearn.datasets
+import sklearn.preprocessing
 
 np.random.seed(42)
 PROJECT_ROOT_DIR = os.getcwd()
@@ -86,4 +87,34 @@ plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo")
 plt.axis([0, 5.5, 0, 2])
 
 save_fig("large_margin_classification_plot")
+
+# # Sensitivity to feature scales
+Xs = np.array([[1, 50], [5, 20], [3, 80], [5, 60]]).astype(np.float64)
+ys = np.array([0, 0, 1, 1])
+svm_clf = sklearn.svm.SVC(kernel="linear", C=100)
+svm_clf.fit(Xs, ys)
+
+plt.figure(figsize=(12, 3.2))
+plt.subplot(121)
+plt.plot(Xs[:, 0][ys == 1], Xs[:, 1][ys == 1], "bo")
+plt.plot(Xs[:, 0][ys == 0], Xs[:, 1][ys == 0], "ms")
+plot_svc_decision_boundary(svm_clf, 0, 6)
+plt.xlabel("$x_0$", fontsize=20)
+plt.ylabel("$x_1$ ", fontsize=20, rotation=0)
+plt.title("Unscaled", fontsize=16)
+plt.axis([0, 6, 0, 90])
+
+scaler = sklearn.preprocessing.StandardScaler()
+X_scaled = scaler.fit_transform(Xs)
+svm_clf.fit(X_scaled, ys)
+
+plt.subplot(122)
+plt.plot(X_scaled[:, 0][ys == 1], X_scaled[:, 1][ys == 1], "bo")
+plt.plot(X_scaled[:, 0][ys == 0], X_scaled[:, 1][ys == 0], "ms")
+plot_svc_decision_boundary(svm_clf, -2, 2)
+plt.xlabel("$x_0$", fontsize=20)
+plt.title("Scaled", fontsize=16)
+plt.axis([-2, 2, -2, 2])
+
+save_fig("sensitivity_to_feature_scales_plot")
 
