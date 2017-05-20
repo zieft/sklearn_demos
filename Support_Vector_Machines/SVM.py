dbb6f2c0e762364b72d5261200003cbb44d8238b
Yulin Zhu <zieft@hotmail.com>
2017-05-20 13:45:02 +0200

    add an example of 3D plotting. The theory of the SVM remains unread

--- Support_Vector_Machines/SVM.py
+++ Support_Vector_Machines/SVM.py
@@ -8,6 +8,7 @@ import sklearn.svm
 import sklearn.datasets
 import sklearn.preprocessing
 import sklearn.pipeline
+import mpl_toolkits.mplot3d
 
 np.random.seed(42)
 PROJECT_ROOT_DIR = os.getcwd()
@@ -510,7 +511,7 @@ def find_support_vectors(svm_reg, X, y):
     """
     y_pred = svm_reg.predict(X)
     off_margin = (np.abs(y - y_pred) >= svm_reg.epsilon)
-    return np.argwhere(off_margin)  # argwhere 这个函数不理解
+    return np.argwhere(off_margin)  # argwhere这个函数不理解
 
 
 svm_reg1.support_ = find_support_vectors(svm_reg1, X, y)
@@ -575,10 +576,62 @@ plt.figure(figsize=(9, 4))
 plt.subplot(121)
 axis_svm_regression_poly = [-1, 1, 0, 1]
 plot_svm_regression(svm_poly_reg1, X, y, axis_svm_regression_poly)
-plt.title("$degree={}, C={}, \epsilon={}$".format(svm_poly_reg1.degree, svm_poly_reg1.C, svm_poly_reg1.epsilon), fontsize=18)
+plt.title("$degree={}, C={}, \epsilon={}$".format(svm_poly_reg1.degree, svm_poly_reg1.C, svm_poly_reg1.epsilon),
+          fontsize=18)
 plt.ylabel("$y$", fontsize=18, rotation=0)
 
 plt.subplot(122)
 plot_svm_regression(svm_poly_reg2, X, y, axis_svm_regression_poly)
-plt.title("$degree={}, C={}, \epsilon={}$".format(svm_poly_reg2.degree, svm_poly_reg2.C, svm_poly_reg2.epsilon), fontsize=18)
+plt.title("$degree={}, C={}, \epsilon={}$".format(svm_poly_reg2.degree, svm_poly_reg2.C, svm_poly_reg2.epsilon),
+          fontsize=18)
 save_fig("svm_with_polynomial_kernel_plot")
+
+# # Under the hook
+iris = sklearn.datasets.load_iris()
+X = iris["data"][:, (2, 3)]  # Only focus on the petal length, and width
+y = (iris["target"] == 2).astype(np.float64)  # Iris-virginica
+
+
+def plot_3D_decision_function(ax, w, b, x1_lim=[4, 6], x2_lim=[0.8, 2.8]):
+    """
+    
+    :param ax: 
+    :param w: 
+    :param b: 
+    :param x1_lim: 
+    :param x2_lim: 
+    :return: 
+    """
+    x1_in_bounds = (X[:, 0] > x1_lim[0]) & (X[:, 0] < x1_lim[1])
+    X_crop = X[x1_in_bounds]
+    y_crop = y[x1_in_bounds]
+    x1s = np.linspace(x1_lim[0], x1_lim[1], 20)
+    x2s = np.linspace(x2_lim[0], x2_lim[1], 20)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    xs = np.c_[x1.ravel(), x2.ravel()]
+    df = (xs.dot(w) + b).reshape(x1.shape)
+    m = 1 / np.linalg.norm(w)
+    boundary_x2s = -x1s * (w[0] / w[1]) - b / w[1]
+    margin_x2s_1 = -x1s * (w[0] / w[1]) - (b - 1) / w[1]
+    margin_x2s_2 = -x1s * (w[0] / w[1]) - (b + 1) / w[1]
+    ax.plot_surface(x1s, x2, 0, color="b", alpha=0.2, cstride=100, rstride=100)
+    ax.plot(x1s, boundary_x2s, 0, "k-", linewidth=2, label=r"$h=0$")
+    ax.plot(x1s, margin_x2s_1, 0, "k--", linewidth=2, label=r"$h=\pm 1$")
+    ax.plot(x1s, margin_x2s_2, 0, "k--", linewidth=2)
+    ax.plot(X_crop[:, 0][y_crop == 1], X_crop[:, 1][y_crop == 1], 0, "g^")
+    ax.plot_wireframe(x1, x2, df, alpha=0.3, color="k")
+    ax.plot(X_crop[:, 0][y_crop == 0], X_crop[:, 1][y_crop == 0], 0, "bs")
+    ax.axis(x1_lim + x2_lim)
+    ax.text(4.5, 2.5, 3.8, "Decision function $h$", fontsize=15)
+    ax.set_xlabel(r"Petal length", fontsize=15)
+    ax.set_ylabel(r"Petal width", fontsize=15)
+    ax.set_zlabel(r"$h = \mathbf{w}^t \cdot \mathbf{x} + b$", fontsize=18)
+    ax.legend(loc="upper left", fontsize=16)
+
+
+fig = plt.figure(figsize=(11, 6))
+ax1 = fig.add_subplot(111, projection="3d")
+plot_3D_decision_function(ax1, w=svm_clf2.coef_[0], b=svm_clf2.intercept_[0])
+
+save_fig("iris_3D_plot")
+
